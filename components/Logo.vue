ab45ffc36a3abd21e7fdcbb93faf5e7a97912603
Syam Pillai <syam.sp0965@gmail.com>
2019-07-14 13:36:15 +0200

    initital commit

---
+++ components/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wedding-logo">
+    <img src="http://www.pngall.com/wp-content/uploads/2016/07/Wedding-PNG-HD.png" alt="">
+  </div>
+</template>
+
+<style>
+.wedding-logo{
+  width: 90%;
+  max-width: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+}
+
+.wedding-logo img{
+    max-width: 100%;
+  }
+</style>
