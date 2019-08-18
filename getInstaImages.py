64313467f0261ae04b8bdd874b96f26aa9a7867b
sarathsp06 <sarathsp06@gmail.com>
2019-08-18 16:38:30 +0200

    fix: fix getInstaImages.py to have uniq images

--- getInstaImages.py
+++ getInstaImages.py
@@ -3,4 +3,6 @@ import re
 import json
 link = "https://www.instagram.com/explore/tags/saraakshi"
 f_name = "dist/_nuxt/instaimages.json"
-json.dump(re.findall(r'https?://\S+?.jpg[^"]+',str(urllib.request.urlopen(link).read())),open(f_name,"w"))
+resultHTML  = str(urllib.request.urlopen(link).read())
+images = sorted(re.findall(r'https?://[^x\s]+?.jpg[^"]+',str(resultHTML)))[::2]
+json.dump(images,open(f_name,"w"))
