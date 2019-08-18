fa9cba0bde4dd6ffbb7ecaead167eb2c732642fa
Sarath Sadasivan Pillai <sarath.sp06@gmail.com>
2019-08-18 15:26:15 +0200

    Update getInstaImages.py

--- getInstaImages.py
+++ getInstaImages.py
@@ -1,9 +1,6 @@
 import urllib.request
 import re
 import json
-link = "https://www.instagram.com/explore/tags/alappuzha"
+link = "https://www.instagram.com/explore/tags/saraakshi"
 f_name = "dist/_nuxt/instaimages.json"
 json.dump(re.findall(r'https?://\S+?.jpg[^"]+',str(urllib.request.urlopen(link).read())),open(f_name,"w"))
-
-
-
